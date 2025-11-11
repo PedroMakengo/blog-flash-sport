f19e07c305c33008d2c26ccbedd3bcc9d64dc981
PedroMakengo <pedromakengomiguel@gmail.com>
2025-11-11 12:38:07 +0000

    Removndo imagens que não estamos utilizando

--- app/components/Header/index.vue
+++ app/components/Header/index.vue
@@ -4,7 +4,7 @@
       class="px-3 md:px-0 max-w-6xl mx-auto flex items-center justify-between"
     >
       <h1 class="text-2xl font-bold text-[#000638] font-merriweather">
-        PopFlash
+        Flash <span class="font-regular font-roboto text-gray-800">Sport</span>
       </h1>
 
       <nav class="hidden md:block font-roboto">
